ab8acddc956b197f48dd916f23581c9f49872a83
Nishi Anand <nishianand.05@gmail.com>
2021-07-27 14:56:13 +0000

    First commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+import MemoryGame from './MemoryGame';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <MemoryGame />
+  </React.StrictMode>
+	,
   document.getElementById('root')
 );
 
